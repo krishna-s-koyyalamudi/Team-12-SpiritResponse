cbeedd52b43d3a86b6b362b7a6563bff24821e1d
Krishna S Koyyalamudi <krishna.kks3797@gmail.com>
2020-11-16 12:34:14 -0600

    Completed spirit from IoT View Controller

--- Spirit Response/Spirit Response/SpiritFromIoTViewController.swift
+++ Spirit Response/Spirit Response/SpiritFromIoTViewController.swift
@@ -10,5 +10,31 @@ import UIKit
 /// SpiritFromIoTViewController class for IoT values tab
 class SpiritFromIoTViewController: UIViewController {
 
-
+    /// rateLBL for the rate of BAC
+    @IBOutlet weak var rateLBL:UILabel!
+    
+    /// stageLBL for the drinking stage
+    @IBOutlet weak var stageLBL:UILabel!
+    
+    /// recordedAtLBL for the date and time recorded
+    @IBOutlet weak var recordedAtLBL:UILabel!
+    
+    /// getSpirit for the getting Spirit value from some API
+    /// - Parameter sender: UIButton
+    @IBAction func getSpirit(sender: Any){
+        //as of now displaying all static data
+        let rate = 10.45
+        let stage = "Sobriety"
+        let recordedAt = "Fri 10:16 PM"
+        rateLBL.text = String(rate)
+        stageLBL.text = stage
+        recordedAtLBL.text = recordedAt
+    }
+    
+    /// viewDidLoad() method
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Spirit from IoT"
+    }
+    
 }
