0bd8aaa597d6a8891c4454e62aa1934f5597f820
Swaroopa tirumalareddy <swaroopatirumalareddy@gmail.com>
2020-11-16 19:27:43 -0600

    working on stop button for stopping the blinking button action

--- Spirit Response/Spirit Response/ResponseViewController.swift
+++ Spirit Response/Spirit Response/ResponseViewController.swift
@@ -24,4 +24,20 @@ class ResponseViewController: UIViewController {
         blinkingButton.isHidden = true
         navigationItem.title = "Try How Fast you React!"
     }
+    /// startBtn to start to display the blinking button to start calculate reaction
+    /// - Parameter sender: UIButton
+    @IBAction func startBtn(_ sender: Any) {
+        blinkingButton.isHidden = false
+        
+        let dateTime = Date()
+        let formatter = DateFormatter()
+        formatter.timeZone = .current
+        formatter.dateFormat = "MM/dd/yyyy HH:mm:ss +SSSS"
+        startTime = formatter.string(from: dateTime as Date)
+    }
+    /// stopBtn for stopping the blinking button action
+    /// - Parameter sender: UIButton
+    @IBAction func stopBtn(_ sender: Any) {
+        blinkingButton.isHidden = true
+    }
 }
