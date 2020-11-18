5e7763d775d17058e61522ce5970d839b1cd5b12
Swaroopa tirumalareddy <swaroopatirumalareddy@gmail.com>
2020-11-17 23:50:47 -0600

    added Variables to change the Text Fields to Doubles

--- Spirit Response/Spirit Response/SpiritCalculatorViewController.swift
+++ Spirit Response/Spirit Response/SpiritCalculatorViewController.swift
@@ -9,5 +9,14 @@ import UIKit
 
 /// SpiritCalculatorViewController class for the calculator view
 class SpiritCalculatorViewController: UIViewController {
+    //Variables to change the Text Fields to Doubles
+      private var weight:Double = 0.0
+      private var beer:Double = 0.0
+      private var wine:Double = 0.0
+      private var liquor:Double = 0.0
+      private var hours:Double = 0.0
+      private var minutes:Double = 0.0
+      private var numResult:String = ""
+      
 
 }
